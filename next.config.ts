106b6f474fbdde3b8eb83a875acb4e63a6816614
haruta.kosukegawa <hako2027@24N3100009Haruta-Kosukegawa.local>
2026-04-18 11:34:02 +0900

    Supabase画像ドメインを追加

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'tsxblhshzukrvppfhujg.supabase.co',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
